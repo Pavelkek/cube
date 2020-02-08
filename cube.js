26fc0055971a1b0265ad5cb4c0abba7619cec400
Pavelkek <ciel30rus@mail.ru>
2020-02-08 22:05:26 +0300

    new project

---
+++ cube.js
@@ -0,0 +1,30 @@
+'use strict';
+const path = require('path');
+const url = require('url');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'cube.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    app.quit();
+});
